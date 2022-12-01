ca5a5422f54cd3afee97837edeb97796db2e8f62
Pohorielova <anna.pogorielova@gmail.com>
2022-12-01 16:54:25 +0100

    Update Transactions

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,32 @@
+import { Box } from '../Box';
+import PropTypes from 'prop-types';
+import {Thead,Tr, Td} from './TransactionHistory.styled';
+export const TransactionHistory =({items})=>{
+  return(
+    <Box as='table' width={350} background='white' borderRadius={6} boxShadow='rgba(0, 0, 0, 0.35) 0px 5px 15px;'>
+  <Thead>
+    <tr>
+      <th>Type</th>
+      <th>Amount</th>
+      <th>Currency</th>
+    </tr>
+  </Thead>
+
+  <tbody>
+  {items.map(({id,type,amount,currency})=>(
+    <Tr key={id}>
+      <Td>{type}</Td>
+    <Td>{amount}</Td>
+      <Td>{currency}</Td>
+    </Tr>
+    ))}
+
+  </tbody>
+</Box>
+  );
+};
+TransactionHistory.prototype = {
+  type: PropTypes.string,
+  amount: PropTypes.number,
+  currency: PropTypes.string,
+};
---
+++ src/components/TransactionHistory/TransactionHistory.styled.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Thead =styled.thead`
+background-color: #aca9a9;
+height: 60px;
+color: white;
+text-transform: uppercase;
+font-size: 15px;
+
+`;
+
+export const Tr =styled.tr`
+background-color: #ffffff;
+height: 40px;
+text-align: center;
+
+`;
+
+export const Td =styled.td`
+background-color:   #edebeb;
+`;
